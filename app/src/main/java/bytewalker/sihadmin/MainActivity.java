1af4e9e0a6998b2f3950839fe0274bcb91cafe07
mayankkasera <mayankkasera98@gmail.com>
2018-03-30 11:15:41 +0530

    first commit

--- app/src/main/java/bytewalker/sihadmin/MainActivity.java
+++ app/src/main/java/bytewalker/sihadmin/MainActivity.java
@@ -3,12 +3,15 @@ package bytewalker.sihadmin;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ////// first commm
+
+
     }
 }
